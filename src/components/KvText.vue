b2577625b54d1b9cf12bd682bcd6a5e300ec2a71
張祐鈞 <zhangyoujun@Mac.home>
2025-05-10 23:27:21 +0800

    fix performance

--- src/components/KvText.vue
+++ src/components/KvText.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, defineProps, onMounted } from 'vue'
-import { onTick, useScreen } from 'vue3-pixi'
+import { onTick, useApplication } from 'vue3-pixi'
 
 const props = defineProps(['x', 'y', 'width', 'text', 'fontSize', 'fontWeight', 'align'])
 const style = {
@@ -18,9 +18,6 @@ const style = {
 const content = props.text.split('')
 const text = ref('')
 const curIdx = ref(0)
-const visible = ref(false)
-const textIns = ref(null)
-const screen = useScreen().value
 
 function getRdnTime({ init = false, pause = false } = {}) {
   return Math.floor(Math.random() * (pause ? 5000 : init ? 6000 : 200)) + (pause ? 5000 : 0);
@@ -37,15 +34,34 @@ function showText() {
   curIdx.value = curIdx.value < content.length ? curIdx.value + 1 : 0
 }
 
-onTick(() => {
-  const bounds = textIns.value.getBounds()
+// 計算可視範圍，判斷要不要 visible 節省 render 資源
+const visible = ref(false)
+const renderer = useApplication().value.renderer
+const canvasW = renderer.width
+const canvasH = renderer.height
+const textIns = ref(null)
 
-  visible.value = !(
-    bounds.x + bounds.width < 0 ||
-    bounds.x > screen.width ||
-    bounds.y + bounds.height < 0 ||
-    bounds.y > screen.height
-  )
+onTick(() => {
+  const screenW = window.innerWidth
+  const screenH = window.innerHeight
+  const scale = Math.max(screenW / canvasW, screenH / canvasH)
+
+  // 實際顯示的大小
+  const displayW = canvasW * scale
+  const displayH = canvasH * scale
+
+  // 被裁掉的上下左右
+  const offsetX = (displayW - screenW) / 2
+  const offsetY = (displayH - screenH) / 2
+
+  const bounds = textIns.value.getBounds()
+  const isVisible =
+    bounds.x + bounds.width > offsetX / scale &&
+    bounds.x < canvasW - offsetX / scale &&
+    bounds.y + bounds.height > offsetY / scale &&
+    bounds.y < canvasH - offsetY / scale
+
+  visible.value = isVisible
 })
 
 onMounted(() => {
