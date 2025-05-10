3554de16afefb1e4cd9e4465647e74d4e62acd96
張祐鈞 <zhangyoujun@Mac.home>
2025-05-10 22:11:22 +0800

    fix performance

--- src/components/KvText.vue
+++ src/components/KvText.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, defineProps, onMounted } from 'vue'
+import { onTick, useScreen } from 'vue3-pixi'
 
 const props = defineProps(['x', 'y', 'width', 'text', 'fontSize', 'fontWeight', 'align'])
 const style = {
@@ -17,6 +18,9 @@ const style = {
 const content = props.text.split('')
 const text = ref('')
 const curIdx = ref(0)
+const visible = ref(false)
+const textIns = ref(null)
+const screen = useScreen().value
 
 function getRdnTime({ init = false, pause = false } = {}) {
   return Math.floor(Math.random() * (pause ? 5000 : init ? 6000 : 200)) + (pause ? 5000 : 0);
@@ -33,6 +37,17 @@ function showText() {
   curIdx.value = curIdx.value < content.length ? curIdx.value + 1 : 0
 }
 
+onTick(() => {
+  const bounds = textIns.value.getBounds()
+
+  visible.value = !(
+    bounds.x + bounds.width < 0 ||
+    bounds.x > screen.width ||
+    bounds.y + bounds.height < 0 ||
+    bounds.y > screen.height
+  )
+})
+
 onMounted(() => {
   setTimeout(showText, getRdnTime({ init: true }))
 })
@@ -40,8 +55,10 @@ onMounted(() => {
 
 <template>
   <Text
+    ref="textIns"
     :x="props.x"
     :y="props.y - 16"
     :text="text"
+    :visible="visible"
     :style="style" />
 </template>
